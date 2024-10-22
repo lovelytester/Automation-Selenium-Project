5eb8793447d0b9824cce289ab429dbd920cb423c
LovelyTester <hi.velea@gmail.com>
2024-10-23 00:40:48 +0700

    - fix test_login_page_negative.py - add pytest-html for reports

--- tests/test_login_page_negative.py
+++ tests/test_login_page_negative.py
@@ -27,7 +27,7 @@ class TestNegativeScenarios:
         # Push Submit button
         submit_button_locator = driver.find_element(By.XPATH, "//button[@class='btn']")
         submit_button_locator.click()
-        #time.sleep(2)
+        time.sleep(2)
 
         # Verify error message is displayed
         error_message_locator = driver.find_element(By.ID, "error")
@@ -36,31 +36,3 @@ class TestNegativeScenarios:
         # Verify error message text is Your username is invalid!
         error_message = error_message_locator.text
         assert error_message == "Your username is invalid!", "Error message is not expected"
-
-    @pytest.mark.login
-    @pytest.mark.negative
-    def test_negative_password(self):
-        # Open page
-        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-        driver.get("https://practicetestautomation.com/practice-test-login/")
-
-        # Type username student into Username field
-        username_locator = driver.find_element(By.ID, "username")
-        username_locator.send_keys("student")
-
-        # Type password incorrectPassword into Password field
-        password_locator = driver.find_element(By.NAME, "password")
-        password_locator.send_keys("incorrectPassword")
-
-        # Push Submit button
-        submit_button_locator = driver.find_element(By.XPATH, "//button[@class='btn']")
-        submit_button_locator.click()
-        #time.sleep(2)
-
-        # Verify error message is displayed
-        error_message_locator = driver.find_element(By.ID, "error")
-        assert error_message_locator.is_displayed(), "Error message is not displayed, but it should be"
-
-        # Verify error message text is Your password is invalid!
-        error_message = error_message_locator.text
-        assert error_message == "Your password is invalid!", "Error message is not expected"

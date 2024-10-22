9163c9ec0db0c4630d039e6653574f5b0addbb33
LovelyTester <hi.velea@gmail.com>
2024-10-23 00:09:26 +0700

    - Move Selenium code from main.py - Integrate Selenium with pytest - Add pytest marks for selective testing - Add pytest.ini to register marks - Add more test cases:   Login positive case   Login negative case - Add requirements.txt

---
+++ tests/test_login_page.py
@@ -0,0 +1,48 @@
+import time
+
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+class TestPositiveScenarios:
+
+    @pytest.mark.login #for selection execute testing
+    @pytest.mark.positive
+    def test_positive_login(self):
+        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+        time.sleep(3)
+
+        # Go to webpage
+        driver.get("https://practicetestautomation.com/practice-test-login/")
+        time.sleep(5)  # Pauses execution for 5 seconds
+
+        # Type username student into Username field
+        username_locator = driver.find_element(By.ID, "username")
+        username_locator.send_keys("student")
+
+        # Type password Password123 into Password field
+        password_locator = driver.find_element(By.NAME, "password")
+        password_locator.send_keys("Password123")
+
+        # Push Submit button
+        submit_button_locator = driver.find_element(By.XPATH, "//button[@class='btn']")
+        submit_button_locator.click()
+        time.sleep(2)
+
+        # Verify new page URL contains practicetestautomation.com/logged-in-successfully/
+        actual_url = driver.current_url
+        # Make comparison to TRUE
+        assert actual_url == "https://practicetestautomation.com/logged-in-successfully/"
+
+        # Verify new page contains expected text ('Congratulations' or 'successfully logged in')
+        text_locator = driver.find_element(By.TAG_NAME, "h1")
+        actual_text = text_locator.text
+        # Make comparison to TRUE
+        assert actual_text == "Logged In Successfully"
+
+        # Verify button Log out is displayed on the new page
+        logout_button_locator = driver.find_element(By.LINK_TEXT, "Log out")
+        assert logout_button_locator.is_displayed()

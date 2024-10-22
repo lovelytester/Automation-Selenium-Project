8d8d14c250656acd9c8a1b6f5d70e3c9861fc4a1
LovelyTester <hi.velea@gmail.com>
2024-10-22 22:31:54 +0700

    Add automation code: positive test case

--- main.py
+++ main.py
@@ -1,11 +1,42 @@
 import time
 
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
 
-# Open Browser Chrome use WebDriver
-driver = webdriver.Chrome()
-time.sleep(2) # Pauses execution for 2 seconds
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+time.sleep(3)
 
-# Navigate to webpage
+
+# Go to webpage
 driver.get("https://practicetestautomation.com/practice-test-login/")
 time.sleep(5) # Pauses execution for 5 seconds
+
+# Type username student into Username field
+username_locator = driver.find_element(By.ID, "username")
+username_locator.send_keys("student")
+
+# Type password Password123 into Password field
+password_locator = driver.find_element(By.NAME, "password")
+password_locator.send_keys("Password123")
+
+# Push Submit button
+submit_button_locator = driver.find_element(By.XPATH, "//button[@class='btn']")
+submit_button_locator.click()
+time.sleep(2)
+
+# Verify new page URL contains practicetestautomation.com/logged-in-successfully/
+actual_url = driver.current_url
+# Make comparison to TRUE
+assert actual_url == "https://practicetestautomation.com/logged-in-successfully/"
+
+# Verify new page contains expected text ('Congratulations' or 'successfully logged in')
+text_locator = driver.find_element(By.TAG_NAME, "h1")
+actual_text = text_locator.text
+# Make comparison to TRUE
+assert actual_text == "Logged In Successfully"
+
+# Verify button Log out is displayed on the new page
+logout_button_locator = driver.find_element(By.LINK_TEXT, "Log out")
+assert logout_button_locator.is_displayed()
